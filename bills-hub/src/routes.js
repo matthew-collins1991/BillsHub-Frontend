72b13419264578d88f3f321a7e08c64d6c9b372d
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-04-03 16:29:28 +0100

    implemented styling

---
+++ bills-hub/src/routes.js
@@ -0,0 +1,52 @@
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import Notifications from "@material-ui/icons/Notifications";
+// core components/views for Admin layout
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserContainer from "views/UserProfile/UserContainer.jsx";
+import CompareContainer from "views/Compare/CompareContainer.jsx";
+import BillContainer from "views/Bills/BillContainer.jsx";
+import NotificationContainer from "views/Notifications/NotificationContainer.jsx";
+
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/bills",
+    name: "Bills",
+    icon: "content_paste",
+    component: BillContainer,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "Profile",
+    icon: Person,
+    component: UserContainer,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: Notifications,
+    component: NotificationContainer,
+    layout: "/admin"
+  },
+  {
+    path: "/compare",
+    name: "Compare",
+    icon: BubbleChart,
+    component: CompareContainer,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
---
+++ bills-hub/src/views/Bills/BillContainer.jsx
@@ -0,0 +1,37 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// core components
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+
+import iconsStyle from "assets/jss/material-dashboard-react/views/iconsStyle.jsx";
+
+function BillContainer(props) {
+  const { classes } = props;
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card plain>
+          <CardHeader plain color="primary">
+            <h4 className={classes.cardTitleWhite}>Bills</h4>
+          </CardHeader>
+          <CardBody>
+            <h3>Bills</h3>
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
+  );
+}
+
+BillContainer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(iconsStyle)(BillContainer);
